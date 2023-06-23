d5ccc81580b1c8cd6f58e235f8b5c5ef5f828a9b
Azain <talpurazain5@gmail.com>
2023-06-24 01:17:48 +0500

    finalCommit

---
+++ Server/Routes/workout.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {
+    createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout, 
+} = require('../Controllers/workoutController')
+const router = express.Router()
+
+//Get all workouts
+router.get('/', getWorkouts)
+
+//Get a single workout
+router.get('/:id', getWorkout)
+
+//Post a new workout
+router.post('/', createWorkout)
+
+//Delete a new workout
+router.delete('/:id', deleteWorkout)
+
+//Update a new workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
